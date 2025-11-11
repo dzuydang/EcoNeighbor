3a5e122bb53328de78c04fd2c6bb7b97220bec00
mrkarkeys <95559518+MrKarkeys@users.noreply.github.com>
2025-11-10 21:05:01 -0800

    completed backend for dashboard page

--- backend/routes/dataRoutes.js
+++ backend/routes/dataRoutes.js
@@ -1,5 +1,21 @@
 import express from "express";
+import {
+  getNumVerifiedReports,
+  getNumUnverifiedReports,
+  getNumReports,
+  getNumAdmin,
+  getNumAuthority,
+  getNumResident,
+} from "../controllers/dataController.js";
+import { verifyToken } from "../utils/verify.js";
 
 const router = express.Router();
+
+router.get("/getNumVerifiedReports", verifyToken, getNumVerifiedReports);
+router.get("/getNumUnverifiedReports", verifyToken, getNumUnverifiedReports);
+router.get("/getNumReports", verifyToken, getNumReports);
+router.get("/getNumAdmin", verifyToken, getNumAdmin);
+router.get("/getNumAuthority", verifyToken, getNumAuthority);
+router.get("/getNumResident", verifyToken, getNumResident);
 
 export default router;
